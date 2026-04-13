eb2595feb88620bc458b3a3974969805782395f6
sumedha-25 <sumedhamehta25@gmail.com>
2026-04-13 11:13:37 +0530

    Coded UC5: Enhanced for loop for multiple names

--- HelloApp.java
+++ HelloApp.java
@@ -1,11 +1,20 @@
 public class HelloApp {
     public static void main(String[] args) {
-        String names;
 
-        if (args.length > 0) {
-            names = String.join(", ", args);
+        StringBuilder names = new StringBuilder();
+
+        if (args.length == 0) {
+            names.append("World");
         } else {
-            names = "World";
+            boolean first = true;
+
+            for (String name : args) {
+                if (!first) {
+                    names.append(", ");
+                }
+                names.append(name);
+                first = false;
+            }
         }
 
         System.out.println("Hello, " + names + "!");
